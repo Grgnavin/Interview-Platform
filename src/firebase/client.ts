e4e4b47a9137b9dc3f3200671886dcec0770edec
navingrg <grgnabin60@gmail.com>
2025-04-13 19:03:50 +0545

    Integrate Firebase and Firestore - Added Firebase and Firebase Admin SDK dependencies in package.json. - Created a new service account JSON file for Firebase Admin SDK. - Implemented Firebase Admin initialization in src/firebase/admin.ts. - Implemented Firebase client initialization in src/firebase/client.ts.

---
+++ src/firebase/client.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp  } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACVj1Scc0BQR2qcF64TpqL3eiWwdfjOio",
+  authDomain: "prepwise-23577.firebaseapp.com",
+  projectId: "prepwise-23577",
+  storageBucket: "prepwise-23577.firebasestorage.app",
+  messagingSenderId: "601749797172",
+  appId: "1:601749797172:web:8d587db7e645ba8ee14d61",
+  measurementId: "G-6QEYGLF27D"
+};
+
+// Initialize Firebase
+const app = !getApps.length ?  initializeApp(firebaseConfig) : getApp();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
